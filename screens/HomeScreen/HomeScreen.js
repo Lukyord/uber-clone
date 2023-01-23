aa175142fd7236f6ef8595fd15cc6543a9ebb4c4
Lukyord <ttansiri524@gmail.com>
2023-01-23 16:42:16 +0100

    add googlePlacesAutoComplete, setOrigin when searc

--- screens/HomeScreen/HomeScreen.js
+++ screens/HomeScreen/HomeScreen.js
@@ -1,8 +1,14 @@
 import { StyleSheet, View, Text, Image } from "react-native";
 import React from "react";
 import NavOptions from "./components/NavOptions";
+import { GOOGLE_MAPS_APIKEY } from "@env";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { useDispatch } from "react-redux";
+import { setDestination, setOrigin } from "../../redux/slices/navSlice";
 
 export default function HomeScreen() {
+  const dispatch = useDispatch();
+
   return (
     <View className="bg-white h-full">
       <View className="p-5">
@@ -16,6 +22,39 @@ export default function HomeScreen() {
             resizeMode: "contain",
           }}
         />
+
+        <GooglePlacesAutocomplete
+          placeholder="Where From?"
+          nearbyPlacesAPI="GooglePlacesSearch"
+          debounce={400}
+          styles={{
+            container: {
+              flex: 0,
+            },
+            textInput: {
+              fontSize: 18,
+            },
+          }}
+          onPress={(data, details = null) => {
+            dispatch(
+              setOrigin({
+                location: details.geometry.location,
+                description: data.description,
+              })
+            );
+
+            dispatch(setDestination(null));
+          }}
+          fetchDetails={true}
+          returnKeyType="search"
+          enablePoweredByContainer={false}
+          minLength={2}
+          query={{
+            key: GOOGLE_MAPS_APIKEY,
+            language: "en",
+          }}
+        />
+
         <NavOptions />
       </View>
     </View>
